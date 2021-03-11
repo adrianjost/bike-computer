e3da19dea3ae2b69a85f83fc7cdb62c12d85b84e
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-03-11 19:36:44 +0100

    ✨ add more speeds to SensorSimulator demo

--- demos/sensorSimulator/sensorSimulator.ino
+++ demos/sensorSimulator/sensorSimulator.ino
@@ -2,16 +2,34 @@
 #define SENSOR_HIGH 30
 #define WHEEL_CIRCUMFERENCE 2100
 
-#define SENSOR_DURATION 378 // ~20km/h
-// #define SENSOR_DURATION 302 // ~25km/h
+#define SENSOR_DURATION_05 1512
+#define SENSOR_DURATION_10 756
+#define SENSOR_DURATION_20 378
+// #define SENSOR_DURATION_25 302
+#define SENSOR_DURATION_30 252
+#define SENSOR_DURATION_40 189
+#define SENSOR_DURATION_50 151
+#define SENSOR_DURATION_60 126
 
 void setup() {
   pinMode(SENSOR_PIN, OUTPUT);
 }
 
-void loop() {
+void simulateSpeed(int roundDuration) {
   digitalWrite(SENSOR_PIN, HIGH);
   delay(SENSOR_HIGH);
   digitalWrite(SENSOR_PIN, LOW);
-  delay(SENSOR_DURATION - SENSOR_HIGH);
+  delay(roundDuration - SENSOR_HIGH);
+}
+
+void loop() {
+  switch((millis() / 10000) % 7){
+    case 0: simulateSpeed(SENSOR_DURATION_05); break;
+    case 1: simulateSpeed(SENSOR_DURATION_10); break;
+    case 2: simulateSpeed(SENSOR_DURATION_20); break;
+    case 3: simulateSpeed(SENSOR_DURATION_30); break;
+    case 4: simulateSpeed(SENSOR_DURATION_40); break;
+    case 5: simulateSpeed(SENSOR_DURATION_50); break;
+    case 6: simulateSpeed(SENSOR_DURATION_60); break;
+  }
 }
