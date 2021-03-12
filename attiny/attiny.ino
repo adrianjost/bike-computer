f1abc7b501fd652af8fe2eb506f061d9908f919c
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-03-12 08:27:08 +0100

    🎨 lint code

--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -3,7 +3,7 @@
 #define SENSOR_INTERRUPT RISING
 #define SENSOR_TRIGGERED HIGH
 #define INTERRUPT_THROTTLE_COOLDOWN 100
-#define VALIDATION_LOOPS 20
+#define VALIDATION_LOOPS 12
 #define INTERRUPT_DEBOUNCE_THRESHOLD 0.75
 
 #define WHEEL_CIRCUMFERENCE 2100
@@ -11,7 +11,10 @@
 #define DATA_SIZE 20
 #define USED_VALUES_FOR_CALC 10
 #define MAX_VALUE_AGE 3000
-#define CLEAR_DATA for(byte i = 0; i < DATA_SIZE; ++i){data[i] = 0;};
+#define CLEAR_DATA                       \
+  for (byte i = 0; i < DATA_SIZE; ++i) { \
+    data[i] = 0;                         \
+  };
 unsigned long data[DATA_SIZE];
 byte lastWriteIndex = 0;
 
@@ -34,7 +37,6 @@ void handleInterrupt() {
   if (!validateSensorState(SENSOR_TRIGGERED)) {
     return;
   }
-
   byte currentWriteIndex = (lastWriteIndex + 1) % DATA_SIZE;
   data[currentWriteIndex] = interruptTime;
   lastWriteIndex = currentWriteIndex;
@@ -47,10 +49,10 @@ void updateSpeed() {
   byte startIndex;
   unsigned long start;
   byte usedValues = 0;
-  for(byte i = 1; i <= USED_VALUES_FOR_CALC; i++){
+  for (byte i = 1; i <= USED_VALUES_FOR_CALC; i++) {
     byte valIndex = (endIndex + DATA_SIZE - i) % DATA_SIZE;
     unsigned long val = data[valIndex];
-    if((end - val) > MAX_VALUE_AGE){
+    if ((end - val) > (MAX_VALUE_AGE)) {
       // value too old
       break;
     }
@@ -58,10 +60,16 @@ void updateSpeed() {
     startIndex = valIndex;
     start = val;
   }
-  #ifdef DEBUG
-  Serial.print("     i "); Serial.print(startIndex); Serial.print(" - "); Serial.println(endIndex);
-  Serial.print("     v "); Serial.print(start); Serial.print(" - "); Serial.println(end);
-  #endif
+#ifdef DEBUG
+  Serial.print("     i ");
+  Serial.print(startIndex);
+  Serial.print(" - ");
+  Serial.println(endIndex);
+  Serial.print("     v ");
+  Serial.print(start);
+  Serial.print(" - ");
+  Serial.println(end);
+#endif
 
   if (start == end || usedValues == 0) {
     speed = 0;
@@ -75,12 +83,12 @@ void setup() {
   Serial.begin(9600);
   pinMode(SENSOR_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), handleInterrupt, SENSOR_INTERRUPT);
-  #ifdef DEBUG
+#ifdef DEBUG
   Serial.print("GO ----------------------- ");
   Serial.print(__DATE__);
   Serial.print(" ");
   Serial.println(__TIME__);
-  #endif
+#endif
 }
 
 void loop() {
