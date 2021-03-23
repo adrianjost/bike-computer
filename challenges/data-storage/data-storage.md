61279be292fe7a6c28870f470e4b056be5909380
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-03-23 14:59:30 +0100

    ✨ sd card server example

--- challenges/data-storage/data-storage.md
+++ challenges/data-storage/data-storage.md
@@ -14,11 +14,100 @@ I want to save data from my trips for further analysis. This should include basi
 | EEProm  | integrated in uC                                          | limited read/write cycles            |
 |         |                                                           | low capacity (0.5-4MB)               |
 
+Problem with SD-Card: I don't have enough PINs on the ESP8266-03 available, except if I can somehow reuse the RX/TX Pins during runtime.
+
+## Code Examples
+
+### SD-Card
+
+```arduino
+/*
+  Listfiles
+
+  This example shows how print out the files in a
+  directory on a SD card
+
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
+  modified 2 Feb 2014
+  by Scott Fitzgerald
+
+  This example code is in the public domain.
+
+*/
+#include <SPI.h>
+#include <SD.h>
+
+File root;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(SS)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  root = SD.open("/");
+
+  printDirectory(root, 0);
+
+  Serial.println("done!");
+}
+
+void loop() {
+  // nothing happens after setup finishes.
+}
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.print(entry.size(), DEC);
+      time_t cr = entry.getCreationTime();
+      time_t lw = entry.getLastWrite();
+      struct tm * tmstruct = localtime(&cr);
+      Serial.printf("\tCREATION: %d-%02d-%02d %02d:%02d:%02d", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+      tmstruct = localtime(&lw);
+      Serial.printf("\tLAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+    }
+    entry.close();
+  }
+}
+```
+
 ## Proposed Data Structure
 
 ## Usefull Links / Tutorials / Resources
 
-REMEMBER!! you can use only 8.3 file, for example a file like config.txt is accepted but configuration.text no, because max file lenght is 8 character and extension 3.
+> ESP8266 comes with it's own SD Lib and the Arduino Default Lib does not compile!
 
 - https://www.mischianti.org/2019/12/15/how-to-use-sd-card-with-esp8266-and-arduino/
 - https://simple-circuit.com/arduino-sd-card-interfacing-example/
--- challenges/data-storage/sd/sd.ino
+++ challenges/data-storage/sd/sd.ino
@@ -1,117 +1,279 @@
 /*
-  SD card test
+ * Copyright (c) 2015, Majenko Technologies
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-  This example shows how use the utility libraries on which the'
-  SD library is based in order to get info about your SD card.
-  Very useful for testing a card when you're not sure whether its working or not.
-
-  The circuit:
-    SD card attached to SPI bus as follows:
- ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module.
- 		Pin 4 used here for consistency with other Arduino examples
-
-
-  created  28 Mar 2011
-  by Limor Fried
-  modified 9 Apr 2012
-  by Tom Igoe
+/* Wir erzeugen einen Web-Server als Access Point, der den Inhalt einer Micro SD Card
+  zuzm Download bereitstellt.
+  Für die SD-Card werden die SPI -Pins benutzt
+  D5 = GPIO14 als Clock
+  D6 = GPIO12 als MISO
+  D7 = GPIO13 als MOSI
+  D8 = GPIO15 als Chip Select
+  
+  die verwendete SSID = SD_Server
+  das Passwort = Micro
+   
 */
-// include the SD library:
-#include <SD.h>
+
+//Wifi Bibliothek + Client + Web-Server
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+//Bibliothek für SPI Bus
 #include <SPI.h>
+//Bibliothek für SD-Card Filesystem
+#include <SdFat.h>
 
-// set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+#include "sdios.h"
 
-// change this to match your SD shield or module;
-// Arduino Ethernet shield: pin 4
-// Adafruit SD shields and modules: pin 10
-// Sparkfun SD shield: pin 8
-// MKRZero SD: SDCARD_SS_PIN
-const int chipSelect = 15;
+using namespace sdfat;
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
+const uint8_t chipSelect = 15;
 
-  Serial.print("\nInitializing SD card...");
+//Template für die HTML-Seite
+const char HTML_HEADER[] =
+    "<!DOCTYPE HTML>"
+    "<html>"
+    "<head>"
+    "<meta name = \"viewport\" content = \"width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0\">"
+    "<title>SD-Card reader</title>"
+    "<style>"
+    "body { background-color: #d2f3eb; font-family: Arial, Helvetica, Sans-Serif; Color: #000000;font-size:12pt; }"
+    "</style>"
+    "</head>"
+    "<body><div style='margin-left:30px;'>";
+const char HTML_END[] =
+    "</div></body>"
+    "</html>";
 
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1)
-      ;
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }
+//Globale Variablen
+//Instanz der SdFat Bibliothek
+SdFat sd;
+//Globale Objekt Variablen zum Speichern von Files und Directories
+SdFile file;
+SdFile dirFile;
+//Access Point
+const char *ssid = "SD_Server";  //Name des WLAN
+const char *password = "Micro";  //Passwort für das WLAN
+//Flag für die SD-Card Initialisierung
+bool sdinit = false;
 
-  // print the type of card
-  Serial.println();
-  Serial.print("Card type:         ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
+ESP8266WebServer server(80);  //Web-Server starten auf Port 80
 
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    while (1)
-      ;
-  }
-
-  Serial.print("Clusters:          ");
-  Serial.println(volume.clusterCount());
-  Serial.print("Blocks x Cluster:  ");
-  Serial.println(volume.blocksPerCluster());
-
-  Serial.print("Total Blocks:      ");
-  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-  Serial.println();
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("Volume type is:    FAT");
-  Serial.println(volume.fatType(), DEC);
-
-  volumesize = volume.blocksPerCluster();  // clusters are collections of blocks
-  volumesize *= volume.clusterCount();     // we'll have a lot of clusters
-  volumesize /= 2;                         // SD card blocks are always 512 bytes (2 blocks are 1KB)
-  Serial.print("Volume size (Kb):  ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mb):  ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Gb):  ");
-  Serial.println((float)volumesize / 1024.0);
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
+//Funktion zum Ermitteln des ContentTypes je nach Datei-Endung
+String getContentType(String filename) {
+  if (server.hasArg("download"))
+    return "application/octet-stream";
+  else if (filename.endsWith(".htm"))
+    return "text/html";
+  else if (filename.endsWith(".html"))
+    return "text/html";
+  else if (filename.endsWith(".css"))
+    return "text/css";
+  else if (filename.endsWith(".js"))
+    return "application/javascript";
+  else if (filename.endsWith(".png"))
+    return "image/png";
+  else if (filename.endsWith(".gif"))
+    return "image/gif";
+  else if (filename.endsWith(".jpg"))
+    return "image/jpeg";
+  else if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  else if (filename.endsWith(".xml"))
+    return "text/xml";
+  else if (filename.endsWith(".pdf"))
+    return "application/x-pdf";
+  else if (filename.endsWith(".zip"))
+    return "application/x-zip";
+  else if (filename.endsWith(".gz"))
+    return "application/x-gzip";
+  return "text/plain";
 }
 
-void loop(void) {
+//Funktion zum Senden einer Datei
+//Parameter Pfad und Dateiname
+bool sendFile(String path, String fn) {
+  char cpath[512];
+  uint32_t filesize;
+  String contentType;
+  char cname[256];
+  sdfat::File myFile;
+
+  //Erstellen des vollständigen Dateinamens
+  //und umspeichern in ein Character-Array
+  path = path + "/" + fn;
+  path.toCharArray(cpath, 512);
+  //Filenamen in Kleinbuchstaben umwandeln
+  //zur Bestimmung des Dateityps
+  fn.toLowerCase();
+  contentType = getContentType(fn);
+  //File auf der SD card öffnen
+  myFile = sd.open(path);
+  //und in den Web-Server streamen
+  server.streamFile(myFile, contentType);
+  //File schließen
+  file.close();
+  return true;
+}
+
+//Ein Dateiverzeichnis senden
+bool sendDirectory(String path) {
+  char cpath[512];
+  uint16_t n = 0;
+  char cname[256];
+  String subdir;
+  String parent;
+  String name;
+
+  path.toCharArray(cpath, 512);
+  //Wir versuchen den Pfad zu öffnen
+  if (dirFile.open(cpath, O_READ)) {
+    //ist die Aktion erfolgreich setzen wir die Contentlänge auf unbekannt
+    server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+    //und senden den Header
+    server.send(200, "text/html", HTML_HEADER);
+    WiFiClient client = server.client();
+    if (path != "/") {
+      //wenn der Pfad nicht auf das Rootverzeichnis zeigt fügen wir eine
+      //Zeile mit ".." ein um eine Ebene zurück gehen zu können
+      parent = path;
+      //wir benötigen das übergeordnete Verzeichnis
+      parent.remove(parent.lastIndexOf("/"));
+      //und bilde daraus einen Link, den wir an den Client senden
+      server.sendContent("<a href='http://192.168.4.1/?DIR=");
+      server.sendContent(parent);
+      server.sendContent("'>..</a><br>");
+    }
+    //nun folgen die Zeilen für die Verzeichniseinträge
+    while (file.openNext(&dirFile, O_RDONLY)) {
+      //am Anfang des Links setzen wir den Pfad
+      server.sendContent("<a href='http://192.168.4.1/?DIR=");
+      server.sendContent(path);
+      //Filename lesen
+      file.getName(cname, 255);
+      name = String(cname);
+      if (file.isDir()) {
+        //wenn der Eintrag ein Unterverzeichnis ist
+        //hängen wir dieses an den Pfad an und schließen den Link
+        subdir = "/" + name;
+        server.sendContent(subdir);
+        server.sendContent("'>");
+      } else {
+        //ist es ein einfaches File fügen wir den Filenamen an
+        server.sendContent("&FN=");
+        server.sendContent(name);
+        //zum Link addieren wir noch "target=''" damit das File
+        //in einem eigenen Fenster geöffnet wird.
+        //Dann schließen wir den Link
+        server.sendContent("' target=''>");
+      }
+      //zum Schluss folgt der Name als Label für den Link
+      server.sendContent(name);
+      //und das Ende vom Link sowie ein Zeilenvorschub
+      server.sendContent("</a><br>");
+      //File schließen
+      file.close();
+    }
+    //Nachdem alle Einträge gesendet wurden, wird das Direktory geschlossen
+    dirFile.close();
+    //Das Contentende dem Client mitteilen und die Verbindung beenden
+    server.sendContent(HTML_END);
+    client.stop();
+    return true;
+  } else {
+    return false;  //Fehler der Pfad konnte nicht geöffnet werden
+  }
+}
+//Diese Funktion wird aufgerufen wenn der Request an den Web-Server = "/" ist
+void handleRoot() {
+  //Filename und Pfad mit Defaultwerten füllen
+  String path = "/";
+  String fn = "";
+  //Falls der Request entsprechende Argumente enthält
+  //Pfad und Filename mit den Daten des Requests füllen
+  if (server.hasArg("DIR")) path = server.arg("DIR");
+  if (server.hasArg("FN")) fn = server.arg("FN");
+  Serial.print("Path ");
+  Serial.print(path);
+  Serial.print(" File: ");
+  Serial.println(fn);
+  String name;
+  //Wenn die SD Card noch nicht initialisiert wurde diese initialisieren
+  if (!sdinit) sdinit = sd.begin(chipSelect, SD_SCK_MHZ(50));
+  if (sdinit) {
+    //War die Initialisierung erfolgreich, lesen wir Daten von der Karte
+    if (fn == "") {
+      //Wenn kein Filename angegeben wurde versuchen wir ein Verzeichnis zu senden
+      if (!sendDirectory(path)) {
+        //Konnte das Verzeichnis nicht gesendet werden setzen wir sdinit auf false
+        //Da offenbar keine Karte im Reader ist. Wir senden an den Client eine entsprechende Warnung
+        sdinit = false;
+        server.send(200, "text/html", "Keine SD Karte");
+      }
+    } else {
+      //Ansonsten versuchen wir das angegebene File zu senden
+      if (!sendFile(path, fn)) {
+        //Konnte das Verzeichnis nicht gesendet werden setzen wir sdinit auf false
+        //Da offenbar keine Karte im Reader ist. Wir senden an den Client eine entsprechende Warnung
+        sdinit = false;
+        server.send(200, "text/html", "Keine SD Karte");
+      }
+    }
+  } else {
+    //Initialisierung nicht erfolgreich
+    server.send(200, "text/html", "Keine SD Karte");
+  }
+}
+
+//Prozessor vorbereiten
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+
+  //Access Point einrichten
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  //ip ist immer 192.168.4.1
+  //Definition der Antwortfunktionen
+  server.on("/", handleRoot);
+  //Web Server starten
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+//Hauptschleife
+void loop() {
+  //Auf Request prüfen
+  server.handleClient();
 }
