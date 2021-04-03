cd7c25423dff1979af5f44ccad286fbc0c2d47bc
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-04-03 12:13:04 +0200

    :fire: remove dead code

--- code/esp8266/esp8266.ino
+++ code/esp8266/esp8266.ino
@@ -110,25 +110,6 @@ volatile unsigned long lastButtonInterrupt = 0;
  UTILS
 **********************************/
 
-void timedLightSleepCallback() {
-  // do nothing
-}
-void timedLightSleep(unsigned int sleepMs) {
-  WiFi.mode(WIFI_OFF);  // you must turn the modem off; using disconnect won't work
-  extern os_timer_t *timer_list;
-  timer_list = nullptr;  // stop (but don't disable) the 4 OS timers
-  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
-#ifdef PIN_BUTTON
-  gpio_pin_wakeup_enable(GPIO_ID_PIN(PIN_BUTTON), WAKEUP_BUTTON);  // (optional)
-#endif
-  wifi_fpm_set_wakeup_cb(timedLightSleepCallback);  // set wakeup callback
-  // the callback is optional, but without it the modem will wake in 10 seconds then delay(10 seconds)
-  // with the callback the sleep time is only 10 seconds total, no extra delay() afterward
-  wifi_fpm_open();
-  wifi_fpm_do_sleep(sleepMs * 1000);  // Sleep range = 10000 ~ 268,435,454 uS (0xFFFFFFE, 2^28-1)
-  delay(sleepMs + 1);                 // delay needs to be 1 mS longer than sleep or it only goes into Modem Sleep
-}
-
 void forcedLightSleep() {
   WiFi.mode(WIFI_OFF);  // you must turn the modem off; using disconnect won't work
   wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
